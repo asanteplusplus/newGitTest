b71ff45c4ce246c6a044856ea03f698aa76cb23f
Alexander Asante <aasante@princeton.edu>
2018-03-22 20:49:20 -0400

    Added Duna.c

---
+++ duna/src/duna.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : duna.c
+ Author      : Alexander Asante
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("First Github Test file"); /* Github Test File */
+	return EXIT_SUCCESS;
+}
